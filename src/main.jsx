f4ce0a99eb5adca1c945d49fedd4fd374d8e7c3c
Simran <simranstha450@gmail.com>
2026-03-24 06:43:37 +0545

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css"; // This imports your styling
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/crud-with-react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
